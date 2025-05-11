3ddf7c68a18749627a24c63167f6be7b8063f106
Alvinnes <pipinwae38@gmail.com>
2025-05-11 22:05:19 +0700

    menambahkan efek radial gradient

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const Content = () => {
   return (
-    <section className="flex w-full flex-wrap justify-center gap-20 py-40">
+    <section className="relative flex w-full flex-wrap justify-center gap-20 py-40">
       <SpaceHouse space="Lampu Kamar 1" />
       <SpaceHouse space="Lampu Kamar 2" />
+      <div className="absolute top-0 -left-10 z-1 size-80 rounded-full bg-slate-200/40 blur-[20rem]"></div>
+      <div className="absolute -right-20 bottom-0 z-1 size-80 rounded-full bg-slate-200/20 blur-[20rem]"></div>
     </section>
   );
 };
@@ -13,7 +15,7 @@ const SpaceHouse = ({ space }) => {
   const [activeLamp, setActiveLamp] = useState(false);
 
   return (
-    <div className="flex w-9/12 flex-col items-center gap-4 rounded-sm bg-slate-700 py-8 text-white shadow-sm">
+    <div className="relative z-3 flex w-9/12 flex-col items-center gap-4 rounded-sm bg-slate-700 py-8 text-white shadow-sm">
       {activeLamp ? (
         <img src="/img/lampu-on.png" alt="" className="size-40" />
       ) : (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 right-0 left-0 flex items-center bg-slate-700 px-[8%] py-6 text-white">
+    <nav className="fixed top-0 right-0 left-0 flex items-center z-999 bg-slate-700 px-[8%] py-6 text-white">
       <h3 className="text-xl font-bold tracking-wide">Kontrol Lampu LED</h3>
     </nav>
   );
